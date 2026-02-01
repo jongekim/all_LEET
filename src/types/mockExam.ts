2f15e0b9bc3263b07a2f751b5a156c48040dc254
jongekim <jongeunkim@kakao.com>
2026-02-01 15:09:12 +0900

    add 프라임

--- src/types/mockExam.ts
+++ src/types/mockExam.ts
@@ -1,9 +1,10 @@
-export type MockExamBaseProvider = '시대인재' | '해커스' | '메가로스쿨';
+export type MockExamBaseProvider = '시대인재' | '해커스' | '메가로스쿨' | '프라임';
 
 export const MOCK_EXAM_BASE_PROVIDERS: MockExamBaseProvider[] = [
   '시대인재',
   '해커스',
   '메가로스쿨',
+  '프라임',
 ];
 
 export interface MockExamSubjectScore {
